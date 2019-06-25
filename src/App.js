8dbe68528a9f490fd5c191d8468a14bf0a0f8e64
Daniel Burnside <burnside@Daniels-MBP.lan1>
2019-06-24 22:55:26 -0500

    Got rid of name in nav for now

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Navigation from '../src/components/Navigation/Navigation.js';
 function App() {
   return (
     <div className="App">
-        <h1 className="name">Daniel Burnside</h1>
         <Navigation></Navigation>
 
     </div>
