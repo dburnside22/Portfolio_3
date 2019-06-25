6a3c60d186c1c7a14f5cf6b77e31da320c6bd742
Daniel Burnside <burnside@Daniels-MBP.lan1>
2019-06-24 22:55:45 -0500

    Added cards to navigate to the other pages

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './Landing.css';
+import { Link } from "react-router-dom";
+
 
 export class Landing extends Component {
     render() {
@@ -8,9 +10,33 @@ export class Landing extends Component {
                 <div className="hero-image">
                     <div className="hero-text">
                         <h1>Daniel Burnside</h1>
-                        <p>Software Developer</p>
+                        <h3>Software Developer</h3>
                     </div>
                 </div>
+                <div className="home-cards">
+                    <div className="card text-center">
+                        <div className="card-body">
+                            <h5 className="card-title">Special title treatment</h5>
+                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <Link to={"/about"} className="btn btn-primary">About</Link>
+                        </div>
+                    </div>
+                    <div className="card text-center">
+                        <div className="card-body">
+                            <h5 className="card-title">Special title treatment</h5>
+                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <Link to={"/projects"} className="btn btn-primary">Projects</Link>
+                        </div>
+                    </div>
+                    <div className="card text-center">
+                        <div className="card-body">
+                            <h5 className="card-title">Special title treatment</h5>
+                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <Link to={"/contact"} className="btn btn-primary">Contact</Link>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         )
     }
