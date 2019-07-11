b37ecd23a0cfe30b542ff7dd2306ee91f91cf139
Dan Burnside <dburnside22@gmail.com>
2019-07-10 19:16:38 -0500

    add super basic contact info

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './Contact.css';
+import photo from './../../../src/computer-picture.jpg';
 
 export class Contact extends Component {
     render() {
         return (
-            <div>
-                
+            <div className='container'>
+                <h1>Contact Me...</h1>
+                <h3>Email: <a href='mailto:dburnside22@gmail.com'>dburnside22@gmail.com</a></h3>
+                <h3><a href='https://www.linkedin.com/in/daniel-burnside/'>LinkedIn</a></h3>
+                <h3><a href='https://github.com/dburnside22'>Github</a></h3>
+                <img className='computer-photo' src={photo}></img>
             </div>
         )
     }
